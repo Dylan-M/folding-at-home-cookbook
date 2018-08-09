26d71bd9af45632f8250657be1e24be57296dff7
Dylan Myers <dylan.myers@bluemedora.com>
2018-08-09 14:49:53 -0400

    Update metadata with long desc and supported operating systems

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Dylan Myers'
 maintainer_email 'ralgith@gmail.com'
 license 'GPL 3.0, See LICENSE file'
 description 'Installs/Configures folding-at-home'
-long_description 'Installs/Configures folding-at-home'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
@@ -18,3 +18,11 @@ issues_url 'https://github.com/Dylan-M/folding-at-home-cookbook/issues'
 # a Supermarket.
 #
 source_url 'https://github.com/Dylan-M/folding-at-home-cookbook'
+
+# Supported OSes
+supports 'centos', '>= 6'
+supports 'redhat', '>= 6'
+supports 'fedora', '>= 25'
+supports 'debian', '>= 9'
+supports 'ubuntu'
+supports 'linuxmint'
