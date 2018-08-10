77d4bdc9d3d1af2faa0c7da2d499a9fbeceb6549
Dylan Myers <dylan.myers@bluemedora.com>
2018-08-10 09:33:57 -0400

    Initial pass at recipe to install on Linux

--- attributes/linux.rb
+++ attributes/linux.rb
@@ -1,10 +1,12 @@
 case node['platform_family']
 when 'debian'
-  default[:fah][:linux][:url][:client] = 'https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.5/fahclient_7.5.1_amd64.deb'
-  default[:fah][:linux][:url][:control] = 'https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.5/fahcontrol_7.5.1-1_all.deb'
-  default[:fah][:linux][:url][:viewer] = 'https://download.foldingathome.org/releases/public/release/fahviewer/debian-stable-64bit/v7.5/fahviewer_7.5.1_amd64.deb'
+  default[:fah][:linux][:url] = 'https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.5/'
+  default[:fah][:linux][:client] = 'fahclient_7.5.1_amd64.deb'
+  default[:fah][:linux][:control] = 'fahcontrol_7.5.1-1_all.deb'
+  default[:fah][:linux][:viewer] = 'fahviewer_7.5.1_amd64.deb'
 when 'rhel', 'fedora'
-  default[:fah][:linux][:url][:client] = 'https://download.foldingathome.org/releases/public/release/fahclient/centos-6.7-64bit/v7.5/fahclient-7.5.1-1.x86_64.rpm'
-  default[:fah][:linux][:url][:control] = 'https://download.foldingathome.org/releases/public/release/fahcontrol/centos-6.7-64bit/v7.5/fahcontrol-7.5.1-1.noarch.rpm'
-  default[:fah][:linux][:url][:viewer] = 'https://download.foldingathome.org/releases/public/release/fahviewer/centos-6.7-64bit/v7.5/fahviewer-7.5.1-1.x86_64.rpm'
+  default[:fah][:linux][:url] = 'https://download.foldingathome.org/releases/public/release/fahclient/centos-6.7-64bit/v7.5/'
+  default[:fah][:linux][:client] = 'fahclient-7.5.1-1.x86_64.rpm'
+  default[:fah][:linux][:control] = 'fahcontrol-7.5.1-1.noarch.rpm'
+  default[:fah][:linux][:viewer] = 'fahviewer-7.5.1-1.x86_64.rpm'
 end
--- recipes/linux.rb
+++ recipes/linux.rb
@@ -3,3 +3,36 @@
 # Recipe:: linux
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+remote_file "/tmp/#{node[:fah][:linux][:client]}" do
+  source "#{node[:fah][:linux][:url]}#{node[:fah][:linux][:client]}"
+  mode '0644'
+  action :create
+end
+
+remote_file "/tmp/#{node[:fah][:linux][:control]}" do
+  source "#{node[:fah][:linux][:url]}#{node[:fah][:linux][:control]}"
+  mode '0644'
+  action :create
+end
+
+remote_file "/tmp/fahviewer#{node[:fah][:linux][:viewer]}" do
+  source "#{node[:fah][:linux][:url]}#{node[:fah][:linux][:viewer]}"
+  mode '0644'
+  action :create
+end
+
+package 'fahclient' do
+  action :upgrade
+  source "/tmp/#{node[:fah][:linux][:client]}"
+end
+
+package 'fahcontrol' do
+  action :upgrade
+  source "/tmp/#{node[:fah][:linux][:control]}"
+end
+
+package 'fahviewer' do
+  action :upgrade
+  source "/tmp/#{node[:fah][:linux][:viewer]}"
+end
